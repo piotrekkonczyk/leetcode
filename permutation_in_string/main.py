02e571a73dea7c2bd391733291b4f51f170e9a4a
piotrekkonczyk <konczyk.piotrek@gmail.com>
2025-07-14 11:35:01 +0200

    permutation in string with fixed sliding window

--- permutation_in_string/main.py
+++ permutation_in_string/main.py
@@ -1,29 +1,33 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s2) < len(s1):
-            return False
-
-        curr_str = ""
-        for i in range(len(s1)):
-            curr_str += s2[i]
-
-        right = len(curr_str)
         left = 0
+        s1_counts = [0] * 26
+        s2_counts = [0] * 26
 
-        s1 = str(sorted(s1))
+        for char in s1:
+            s1_counts[ord(char) - 97] += 1  # 97 is lowercase "a"
 
-        while right < len(s2):
-            if str(sorted(curr_str)) == s1:
+        for right in range(len(s2)):
+            width = right - left + 1
+
+            if width < len(s1):
+                s2_counts[ord(s2[right]) - 97] += 1
+                continue
+
+            if left > 0:
+                s2_counts[ord(s2[left - 1]) - 97] -= 1
+
+            s2_counts[ord(s2[right]) - 97] += 1
+
+            if s1_counts == s2_counts:
                 return True
-            else:
-                right += 1
-                left += 1
-                curr_str = s2[left:right]
 
-        return str(sorted(curr_str)) == s1
+            left += 1
+
+        return False
 
 
 solution = Solution()
 print(solution.checkInclusion(s1="ab", s2="eidbaooo"))
-print(solution.checkInclusion(s1="ab", s2="eidboaoo"))
-print(solution.checkInclusion(s1="adc", s2="dcda"))
+print(solution.checkInclusion(s1="hello", s2="ooolleoooleh"))
+print(solution.checkInclusion(s1="abc", s2="cccccbabbbaaaa"))
